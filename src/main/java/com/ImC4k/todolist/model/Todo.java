c378509697dc457de3d25ca15b9eb7bcb8f931d0
Calvin Chu <calvinchu26@gmail.com>
2020-12-10 19:40:12 +0800

    [api][Calvin] fix: Todo setters

--- src/main/java/com/ImC4k/todolist/model/Todo.java
+++ src/main/java/com/ImC4k/todolist/model/Todo.java
@@ -34,16 +34,32 @@ public class Todo {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getText() {
         return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 
     public Boolean getDone() {
         return done;
     }
 
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
     public List<String> getLabelIds() {
         return labelIds;
+    }
+
+    public void setLabelIds(List<String> labelIds) {
+        this.labelIds = labelIds;
     }
 
     @Override

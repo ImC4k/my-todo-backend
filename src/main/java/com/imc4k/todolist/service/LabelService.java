578c8a3e78883f17f21838112662763f98a75ec9
Calvin Chu <calvinchu26@gmail.com>
2020-12-10 17:21:34 +0800

    [api][Calvin] test+feat: LabelService getAll

--- src/main/java/com/imc4k/todolist/service/LabelService.java
+++ src/main/java/com/imc4k/todolist/service/LabelService.java
@@ -1,11 +1,18 @@
 package com.imc4k.todolist.service;
 
+import com.imc4k.todolist.model.Label;
 import com.imc4k.todolist.repository.LabelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LabelService {
     @Autowired
     private LabelRepository labelRepository;
+
+    public List<Label> getAll() {
+        return labelRepository.findAll();
+    }
 }
--- src/test/java/com/imc4k/todolist/service/LabelServiceTest.java
+++ src/test/java/com/imc4k/todolist/service/LabelServiceTest.java
@@ -1,5 +1,6 @@
 package com.imc4k.todolist.service;
 
+import com.imc4k.todolist.model.Label;
 import com.imc4k.todolist.repository.LabelRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,6 +8,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -16,15 +23,27 @@ public class LabelServiceTest {
 
     @Mock
     private LabelRepository labelRepository;
-    
+
+    private List<Label> createDummyLabels() {
+        List<Label> allLabels = new ArrayList<>();
+        allLabels.add(new Label("1", "label1", "#FFFFFF"));
+        allLabels.add(new Label("2", "label2", "#FFFFFF"));
+        allLabels.add(new Label("3", "label3", "#FFFFFF"));
+        allLabels.add(new Label("4", "label4", "#FFFFFF"));
+        allLabels.add(new Label("5", "label5", "#FFFFFF"));
+        return allLabels;
+    }
+
     @Test
-    void should__when__given_() {
+    void should_return_all_labels_when_getAll_given_labels_not_empty() {
         //given
-        
+        final List<Label> expected = createDummyLabels();
+        when(labelRepository.findAll()).thenReturn(expected);
                 
         //when
-        
+        final List<Label> actual = labelService.getAll();
         
         //then
+        assertEquals(createDummyLabels().size(), actual.size());
     }
 }

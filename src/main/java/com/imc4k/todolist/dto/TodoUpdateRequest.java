54e326da4e713a92d7221cd2fc919685393776dd
Calvin Chu <calvinchu26@gmail.com>
2020-12-10 20:02:20 +0800

    [api][Calvin] TodoUpdateRequest add setters

--- src/main/java/com/imc4k/todolist/dto/TodoUpdateRequest.java
+++ src/main/java/com/imc4k/todolist/dto/TodoUpdateRequest.java
@@ -11,11 +11,23 @@ public class TodoUpdateRequest {
         return text;
     }
 
+    public void setText(String text) {
+        this.text = text;
+    }
+
     public Boolean getDone() {
         return done;
     }
 
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
     public List<String> getLabelIds() {
         return labelIds;
+    }
+
+    public void setLabelIds(List<String> labelIds) {
+        this.labelIds = labelIds;
     }
 }

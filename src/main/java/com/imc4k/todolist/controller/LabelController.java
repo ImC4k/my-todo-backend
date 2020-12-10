3d374960d6a7964b34ddfc80e148cf29bc2e1de3
Calvin Chu <calvinchu26@gmail.com>
2020-12-10 20:34:51 +0800

    [api][Calvin] fix: cross origin issue

--- src/main/java/com/imc4k/todolist/controller/LabelController.java
+++ src/main/java/com/imc4k/todolist/controller/LabelController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "localhost:3000")
+@CrossOrigin
 @RestController
 @RequestMapping("/labels")
 public class LabelController {
